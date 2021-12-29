21d7ffce9cb3986224ae88d71e0a8d7312616a4f
ishaanp9 <41341006+ishaanp9@users.noreply.github.com>
2021-12-28 16:58:25 -0800

    added whole team functionality, and added mongodb client access

---
+++ db/_init_.py
@@ -0,0 +1,13 @@
+import os
+import pymongo
+
+# Playing Around with Mongodb
+
+client = pymongo.MongoClient(“mongodb://localhost:27017/”)
+db = client[“family_data”]
+my_collection = db["family_members"]
+
+mydict = {"name": "Ishaan" "age": 19}
+x = mycollection.insert_one(mydict)
+
+print(x.inserted_id)
